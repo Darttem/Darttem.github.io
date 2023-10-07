f002bc48ae8fdfda74cc0850feea8b82329a317a
Darttem <artemzhdan1979@gmail.com>
2023-10-07 13:14:07 +0300

    sdhk

--- script.js
+++ script.js
@@ -9,15 +9,12 @@ let changeLock = false;
 let x;
 let d;
 
-
-let highScore = localStorage.getItem("highScore") || 0;
-document.getElementById("record").innerText = highScore;
-
+let record = localStorage.getItem("record") || 0;
+document.getElementById("record").innerText = record;
 
 let score = 0;
 let speed = 100; // default speed
 let color = 'red'; // default color
-let record = 0;
 
 let snake = [];
 snake[0] = { x: 10 * box, y: 10 * box };
@@ -27,7 +24,6 @@ let food = {
   y: Math.floor(Math.random() * canvasSize) * box,
 };
 
-
 let bodyTexture = new Image();
 bodyTexture.src = "images/body.jpg";
 
@@ -36,6 +32,20 @@ headTexture.src = "images/head.jpg";
 
 let tailTexture = new Image();
 tailTexture.src = "images/tail.jpg";
+
+
+
+
+let visits = localStorage.getItem("visits") || 0;
+
+// Збільшуємо лічильник на один
+visits++;
+
+// Зберігаємо нове значення у localStorage
+localStorage.setItem("visits", visits);
+
+// Відображаємо кількість відвідувань на сторінці
+document.getElementById("visitsCount").innerText = visits;
 
 
 
@@ -50,53 +60,30 @@ function restartGame() {
         x: Math.floor(Math.random() * canvasSize) * box,
         y: Math.floor(Math.random() * canvasSize) * box,
     };
-    
-    
 }
 
-
-
 document.addEventListener("keydown", direction);
-
-
 
 function changeDirection(newDirection) {
     if (newDirection === 'UP' && d !== 'DOWN') d = 'UP';
     if (newDirection === 'DOWN' && d !== 'UP') d = 'DOWN';
     if (newDirection === 'LEFT' && d !== 'RIGHT') d = 'LEFT';
     if (newDirection === 'RIGHT' && d !== 'LEFT') d = 'RIGHT';
-  }
-
-  
-
+}
 
 function direction(event) {
     if (changeLock) return;
     changeLock = true;
 
-    document.getElementById("left").addEventListener("click", function () {
-        if (d !== "RIGHT") d = "LEFT";
-      });
-      document.getElementById("up").addEventListener("click", function () {
-        if (d !== "DOWN") d = "UP";
-      });
-      document.getElementById("right").addEventListener("click", function () {
-        if (d !== "LEFT") d = "RIGHT";
-      });
-      document.getElementById("down").addEventListener("click", function () {
-        if (d !== "UP") d = "DOWN";
-      });
-      
-
-  if (event.key === "ArrowLeft" && d !== "RIGHT") d = "LEFT";
-  else if (event.key === "ArrowUp" && d !== "DOWN") d = "UP";
-  else if (event.key === "ArrowRight" && d !== "LEFT") d = "RIGHT";
-  else if (event.key === "ArrowDown" && d !== "UP") d = "DOWN";
-  // for AWSD controls
-  else if (event.key === "a" && d !== "RIGHT") d = "LEFT";
-  else if (event.key === "w" && d !== "DOWN") d = "UP";
-  else if (event.key === "d" && d !== "LEFT") d = "RIGHT";
-  else if (event.key === "s" && d !== "UP") d = "DOWN";
+    if (event.key === "ArrowLeft" && d !== "RIGHT") d = "LEFT";
+    else if (event.key === "ArrowUp" && d !== "DOWN") d = "UP";
+    else if (event.key === "ArrowRight" && d !== "LEFT") d = "RIGHT";
+    else if (event.key === "ArrowDown" && d !== "UP") d = "DOWN";
+    // for AWSD controls
+    else if (event.key === "a" && d !== "RIGHT") d = "LEFT";
+    else if (event.key === "w" && d !== "DOWN") d = "UP";
+    else if (event.key === "d" && d !== "LEFT") d = "RIGHT";
+    else if (event.key === "s" && d !== "UP") d = "DOWN";
 }
 
 document.getElementsByName("speed").forEach(radio => {
@@ -134,26 +121,12 @@ function updateScore() {
     document.getElementById("score").innerText = score;
     if (score > record) {
         record = score;
+        localStorage.setItem("record", record);
         document.getElementById("record").innerText = record;
     }
 }
 
-bodyTexture.onload = function() {
-    // Якщо потрібно, виконайте який-небудь код тут
-};
-
-headTexture.onload = function() {
-    // Якщо потрібно, виконайте який-небудь код тут
-};
-
-tailTexture.onload = function() {
-    // Якщо потрібно, виконайте який-небудь код тут
-};
-
-
 function draw() {
-
-    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     for (let i = 0; i < snake.length; i++) {
@@ -166,38 +139,16 @@ function draw() {
         }
     }
     
-    
-    highScore = record;
-    localStorage.setItem("highScore", highScore);
-    document.getElementById("record").innerText = highScore;
-
-    for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = i === 0 ? color : 'white';
-        ctx.fillRect(snake[i].x, snake[i].y, box, box);
-        ctx.strokeStyle = "black";
-        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
-    }
-
-    
-
     ctx.fillStyle = 'red';
     ctx.fillRect(food.x, food.y, box, box);
 
-    
-
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
-
-    
 
     if (d === "LEFT") snakeX -= box;
     if (d === "UP") snakeY -= box;
     if (d === "RIGHT") snakeX += box;
     if (d === "DOWN") snakeY += box;
-
-
-    
-
 
     // Телепортація
     if (snakeX < 0) snakeX = canvas.width - box;
@@ -228,19 +179,10 @@ function draw() {
         restartGame();
     }
 
-
-    
-    
-
     snake.unshift(newHead);
-
-    
 
     updateScore();
     changeLock = false;
-
-    
-
 }
 
 let game = setInterval(draw, speed);
